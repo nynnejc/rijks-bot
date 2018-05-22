e436aae29c7d54bdd0189ce78ad7d5399e64f148
Tonio Serna <antonelo@gmail.com>
2018-05-23 00:10:30 +0200

    Set minitest-reporters

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,6 @@
 ENV["RAILS_ENV"] = "test"
-
+require "minitest/reporters"
+Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(:color => true)]
 require "minitest/autorun"
 require 'support/vcr'
 
